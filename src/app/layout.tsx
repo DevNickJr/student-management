b4b23e1d816eb51015bed5a006a1c239fdf062fb
DevNickJr <nicholasduadei14@gmail.com>
2023-07-04 19:36:11 +0100

    next auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
+import NextAuthSesssionProvider from '@/providers/SesssionProvider';
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { ToastContainer } from 'react-toastify';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <NextAuthSesssionProvider>
+        {children}
+        <ToastContainer />
+      </NextAuthSesssionProvider>
+      </body>
     </html>
   )
 }

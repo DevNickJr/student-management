b04e3148a4d184a2eb9225c9d564edff40493a88
DevNickJr <nicholasduadei14@gmail.com>
2023-07-09 10:52:27 +0100

    update profile

--- src/app/(student)/dashboard/page.tsx
+++ src/app/(student)/dashboard/page.tsx
@@ -1,17 +1,56 @@
 'use client'
 import React from 'react'
-import { MdAdd } from 'react-icons/md'
 import { useSession } from 'next-auth/react'
+import { MdAdd } from 'react-icons/md'
+import { BsFillPersonFill, BsBarChartFill } from 'react-icons/bs'
+import { FaSchool} from 'react-icons/fa'
+import { BiSolidSchool} from 'react-icons/bi'
+import { LiaSchoolSolid } from 'react-icons/lia'
+import { GoNumber } from 'react-icons/go'
+import { CgNotes } from 'react-icons/cg'
+import { RiGraduationCapFill } from 'react-icons/ri'
+
+const userData = [
+  {
+    accessor: ['last_name', 'first_name', 'middle_name'],
+    label: 'Name',
+    Icon: BsFillPersonFill,
+  },
+  {
+    accessor: 'email',
+    label: 'Email',
+    Icon: BsFillPersonFill,
+  },
+  {
+    accessor: 'phone',
+    label: 'Phone',
+    Icon: BsFillPersonFill,
+  },
+  {
+    accessor: 'level',
+    label: 'Level',
+    Icon: BsFillPersonFill,
+  },
+  {
+    accessor: 'matric_no',
+    label: 'Matric Number',
+    Icon: BsFillPersonFill,
+  },
+  {
+    accessor: 'is_verified',
+    label: 'Verified',
+  }
+]
 
 const StudentHome = () => {
-  const session = useSession()
+  const { data } = useSession()
 
-  console.log("dash session", session)
+  // console.log("dash session", data)
   return (
     <div className='p-4 overflow-y-auto'>
       <div className="flex items-center gap-4 justify-between mb-12">
         <div className="flex flex-col gap-2">
-          <h1 className='text-2xl font-bold'>Welcome Back, Jonathan</h1>
+          <h1 className='text-2xl font-bold'>Welcome Back, {data?.user?.userDetails?.first_name}</h1>
           <p className='text-sm'>You can easily register your courses on Eduverse</p>
         </div>
         <button className='flex items-center gap-2 bg-primary p-2 pr-3 text-sm text-white'>
@@ -19,20 +58,85 @@ const StudentHome = () => {
           Register Course
         </button>
       </div>
-      <div className="bg-white p-4 pb-12 rounded-md">
-        <h3 className="text-lg mb-8">
+      <div className="bg-white p-4 md:p-8 pb-12 rounded-md">
+        <h3 className="text-lg mb-8 md:mb-12 font-semibold">
           User Data
         </h3>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
-          {[0,1,2,3,4,5,6].map((_, i) => (
-          <div key={i} className='flex items-center gap-2 text-xs'>
-            <div className="w-10 h-10 rounded-full bg-gray-200" />
-            <div className="flex flex-col gap-1">
-             <div>Nicholas Duadei</div>
-             <div>nicholasduadei14@gmail.com</div>
+
+          <div className='flex items-center gap-4 text-xs'>
+            <div className="w-12 h-12 rounded-full bg-[#D4E5F7] flex justify-center items-center">
+              <BsFillPersonFill className='text-3xl text-[#1B5390]' />
+            </div>
+            <div className="flex flex-col gap-2">
+             <div className='font-medium'>Full Names</div>
+             <div className='text-[11px] text-black/60'>{data?.user?.userDetails?.last_name} {data?.user?.userDetails?.first_name} {data?.user?.userDetails?.middle_name}</div>
             </div>
           </div>
-          ))}
+          <div className='flex items-center gap-2 text-xs'>
+             <div className="w-12 h-12 rounded-full bg-[#D4E5F7] flex justify-center items-center">
+              <GoNumber className='text-3xl text-[#1B5390]' />
+            </div>
+            <div className="flex flex-col gap-2">
+             <div className='font-medium'>Current Level</div>
+             <div className='text-[11px] text-black/60'>{data?.user?.userDetails?.level}</div>
+            </div>
+          </div>
+          <div className='flex items-center gap-2 text-xs'>
+             <div className="w-12 h-12 rounded-full bg-[#D4E5F7] flex justify-center items-center">
+              <GoNumber className='text-3xl text-[#1B5390]' />
+            </div>
+            <div className="flex flex-col gap-2">
+             <div className='font-medium'>Matric Number</div>
+             <div className='text-[11px] text-black/60'>{data?.user?.userDetails?.matric_no}</div>
+            </div>
+          </div>
+          <div className='flex items-center gap-2 text-xs'>
+             <div className="w-12 h-12 rounded-full bg-[#D4E5F7] flex justify-center items-center">
+              <RiGraduationCapFill className='text-3xl text-[#1B5390]' />
+            </div>
+            <div className="flex flex-col gap-2">
+             <div className='font-medium'>Student Type</div>
+             <div className='text-[11px] text-black/60'>Undergraduate</div>
+            </div>
+          </div>
+          <div className='flex items-center gap-2 text-xs'>
+             <div className="w-12 h-12 rounded-full bg-[#D4E5F7] flex justify-center items-center">
+              <BiSolidSchool className='text-3xl text-[#1B5390]' />
+            </div>
+            <div className="flex flex-col gap-2">
+             <div className='font-medium'>Department</div>
+             <div className='text-[11px] text-black/60'>EEE</div>
+            </div>
+          </div>
+          <div className='flex items-center gap-2 text-xs'>
+             <div className="w-12 h-12 rounded-full bg-[#D4E5F7] flex justify-center items-center">
+              <LiaSchoolSolid className='text-3xl text-[#1B5390]' />
+            </div>
+            <div className="flex flex-col gap-2">
+             <div className='font-medium'>Option</div>
+             <div className='text-[11px] text-black/60'>ECE</div>
+            </div>
+          </div>
+          <div className='flex items-center gap-2 text-xs'>
+             <div className="w-12 h-12 rounded-full bg-[#D4E5F7] flex justify-center items-center">
+              <FaSchool className='text-3xl text-[#1B5390]' />
+            </div>
+            <div className="flex flex-col gap-2">
+             <div className='font-medium'>Faculty</div>
+             <div className='text-[11px] text-black/60'>SSCE</div>
+            </div>
+          </div>
+          <div className='flex items-center gap-2 text-xs'>
+             <div className="w-12 h-12 rounded-full bg-[#D4E5F7] flex justify-center items-center">
+              <CgNotes className='text-3xl text-[#1B5390]' />
+            </div>
+            <div className="flex flex-col gap-2">
+             <div className='font-medium'>Session</div>
+             <div className='text-[11px] text-black/60'>2020/2021</div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
--- src/app/(student)/profile/page.tsx
+++ src/app/(student)/profile/page.tsx
@@ -1,7 +1,81 @@
+'use client'
+import useFetch from '@/hooks/useFetch'
+import usePost from '@/hooks/usePost'
+import { IProfile, IReducerAction, IUser } from '@/interfaces'
+import { apiGetUser, apiUpdateUser, apiUpdateProfileImage } from '@/services/AuthService'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import { MdAdd } from 'react-icons/md'
+import { toast } from 'react-toastify'
+
+
+
+const initialState: IProfile = {
+  email: '',
+  phone: '',
+  first_name: '',      
+  last_name: '',        
+  level: '',      
+  matric_no: '',  
+}
+
+export interface IProfileReducerAction extends IReducerAction<"email"  | "phone" | "first_name" | "last_name" | "level" | "matric_no"> {
+  payload: string
+}
+
 
 const StudentProfile = () => {
+  const imageRef = React.useRef<HTMLInputElement>(null)
+  const [user, dispatch] = React.useReducer((state: IProfile, action: IProfileReducerAction) => {
+    return { ...state, [action.type]: action.payload }
+}, initialState)
+  const { data: profile, error, isLoading, isFetching, remove, refetch, fetchStatus } = useFetch<IProfile>({api: apiGetUser, requireAuth: true, key: ['user', 'profile'] })
+  const updateProfileMutation = usePost<IProfile, IProfile>(apiUpdateUser, { 
+    requireAuth: true,
+    onSuccess: (data) => {
+      console.log("data", data)
+      refetch()
+    }
+  })
+  
+  const profilePhotoMutation = usePost<FormData, FormData>(apiUpdateProfileImage, { 
+    requireAuth: true,
+    onSuccess: (data) => {
+      console.log("data", data)
+      refetch()
+    }
+  })
+
+  React.useEffect(() => {
+    if(profile) {
+      dispatch({type: 'email', payload: profile?.email})
+      dispatch({type: 'phone', payload: profile?.phone})
+      dispatch({type: 'first_name', payload: profile?.first_name})
+      dispatch({type: 'last_name', payload: profile?.last_name})
+      dispatch({type: 'level', payload: profile?.level})
+      dispatch({type: 'matric_no', payload: profile?.matric_no})
+    }
+  }, [profile])
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log("user", user)
+    updateProfileMutation.mutate(user)
+  }
+
+  const changeDP = () => {
+    const file = imageRef.current?.files?.[0]
+    if(file) {
+      const formData = new FormData()
+      formData.append('profile_picture', file)
+      profilePhotoMutation.mutate(formData)
+    }
+  }
+
+
+
+  // console.log("profile", profile)
   return (
     <div className='p-4 overflow-y-auto'>
       <div className="flex items-center gap-4 justify-between mb-12">
@@ -12,26 +86,47 @@ const StudentProfile = () => {
           Profile Image
         </h3>
         <div className='flex items-center gap-6 text-xs my-8'>
-            <div className="w-24 h-24 rounded-full bg-gray-200" />
+            <div className="w-24 h-24 rounded-full bg-gray-200 flex justify-center items-center">
+              <Image src={profile?.profile_picture_url || ''} alt='profile photo' width={100} height={100} className='rounded-full w-full h-full' />
+            </div>
             <div className="flex flex-col gap-2">
-            <button className='flex items-center justify-center gap-2 bg-primary p-2 pl-5 pr-6 text-sm text-white'>
+            <label htmlFor='dp' className='flex items-center justify-center gap-2 bg-primary p-2 pl-5 pr-6 text-sm text-white'>
               <MdAdd className='text-2xl' />
               Change
-            </button>
+            </label>
+            <input name='dp' id='dp' type='file' onChange={changeDP} ref={imageRef} className='hidden' />
             <button className='flex items-center justify-center gap-2 p-2 pl-5 pr-6 text-sm text-red-500 border bg-gray-50'>
               <MdAdd className='text-2xl' />
               Delete
             </button>
             </div>
           </div>
-        <form className="mb-8">
+        <form className="mb-8 text-sm" onSubmit={handleSubmit}>
           <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-10'>
-            {[0,1,2,3,4,5].map((_, i) => (
-            <div key={i} className='flex flex-col gap-2 text-xs'>
-              <label htmlFor="name">First Name</label>
-              <input type="text" name="name" id="name" className='border p-2 rounded-md' />
+            <div className='flex flex-col gap-2 text-xs'>
+              <label htmlFor="first_name">First Name</label>
+              <input type="text" name="first_name" id="first_name" className='border p-2 rounded-md text-sm' value={user?.first_name} onChange={(e) => dispatch({type: 'first_name', payload: e.target.value })} />
             </div>
-            ))}
+            <div className='flex flex-col gap-2 text-xs'>
+              <label htmlFor="last_name">Last Name</label>
+              <input type="text" name="last_name" id="last_name" className='border p-2 rounded-md' value={user?.last_name} onChange={(e) => dispatch({type: 'last_name', payload: e.target.value })} />
+            </div>
+            <div className='flex flex-col gap-2 text-xs'>
+              <label htmlFor="email">Email Address</label>
+              <input type="email" name="email" id="email" className='border p-2 rounded-md' value={user?.email} onChange={(e) => dispatch({type: 'email', payload: e.target.value })} />
+            </div>
+            <div className='flex flex-col gap-2 text-xs'>
+              <label htmlFor="phone">Phone Number</label>
+              <input type="tel" name="phone" id="phone" className='border p-2 rounded-md' value={user?.phone} onChange={(e) => dispatch({type: 'phone', payload: e.target.value })} />
+            </div>
+            <div className='flex flex-col gap-2 text-xs'>
+              <label htmlFor="level">Level</label>
+              <input type="text" name="level" id="level" className='border p-2 rounded-md' value={user?.level} onChange={(e) => dispatch({type: 'level', payload: e.target.value })} />
+            </div>
+            <div className='flex flex-col gap-2 text-xs'>
+              <label htmlFor="matric">Matric Number</label>
+              <input type="text" name="matric" id="matric" className='border p-2 rounded-md' value={user?.matric_no} onChange={(e) => dispatch({type: 'matric_no', payload: e.target.value })} />
+            </div>
           </div>
           <button type='submit' className='mt-12 flex items-center justify-center gap-2 bg-primary p-2 pl-5 pr-6 text-sm text-white'>
             Save Change
@@ -41,9 +136,9 @@ const StudentProfile = () => {
           <div className='bg-gray-200 rounded-md p-4 flex-1'>
             <div className="flex justify-between gap-4 items-center mb-2">
               <h6 className='font-semibold text-sm'>Password</h6>
-              <button className='bg-white p-1 px-2 text-[10px] text-primary'>
+              <Link href={'/profile/password'} className='bg-white p-1 px-2 text-[10px] text-primary'>
                 Change
-              </button>
+              </Link>
             </div>
             <p>You can reset or change your password by clicking here</p>
           </div>

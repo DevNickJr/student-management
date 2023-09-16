d06662acb8f0dcce20901f53fb96e7ae0939635e
DevNickjr <nicholasduadei14@gmail.com>
2023-09-16 07:58:45 +0100

    register courses

---
+++ src/app/(student)/courses/confirm/Confirm.tsx
@@ -0,0 +1,245 @@
+'use client'
+import React from 'react'
+import { MdAdd } from 'react-icons/md'
+import { IProfile, ITableColumn } from '@/interfaces'
+import { apiGetUser } from '@/services/AuthService'
+import useFetch from '@/hooks/useFetch'
+import { apiFilterCourses, apiRegisterCourses } from '@/services/UserService'
+import Table from '@/components/Table/CourseReg'
+import usePost from '@/hooks/usePost'
+import { toast } from 'react-toastify'
+import Loader from '@/components/Loader'
+import CheckImg from "@/assets/check.png"
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import Link from 'next/link'
+import { ICourse } from '@/interfaces'
+
+
+interface IProps {
+  details: ICourse
+}
+
+const ConfirmCourse = ({ details }: IProps) => {
+  const { data, error, isLoading, isFetching, fetchStatus } = useFetch<IProfile>({api: apiGetUser, requireAuth: true, key: ['user', 'profile'] })
+  const router = useRouter()
+  const [modalOpen, setModalOpen] = React.useState(false)
+
+  const registerCoursesMutation = usePost<any, any>(
+    apiRegisterCourses,
+    {
+      showSuccessMessage: false,
+      showErrorMessage: false,
+      requireAuth: true,
+      onSuccess: (data) => {
+          toast.success(data?.data?.message)
+          setModalOpen(true)
+          console.log(data?.message || '')
+      },
+      onError: (error: any) => {
+        console.log({error})
+        toast.error(error?.response?.data?.message || "An error occured")
+      }
+    }
+  )
+
+  const [courses, setCourses] = React.useState<{ id: string, register: boolean }[]>([])
+
+  const handleSelect = (value: any) => {
+    setCourses(courses => courses.map((course) => {
+      if (course.id != value.id) {
+        return course
+      }
+      return {
+        ...course,
+        register: !course.register
+      }
+    }))
+  }
+
+  const handleRegister = () => {
+    const registered = courses.filter(course => course.register)
+    const course_ids = registered.map(course => course.id)
+
+    if (!course_ids.length) {
+      return toast.info("You must select courses to register")
+    }
+
+    registerCoursesMutation.mutate({
+      course_ids
+    })
+  }
+
+  const { data: filteredCourses, isLoading: filteredCoursesLoading } = useFetch({
+    api: apiFilterCourses, 
+    param: {
+      level: details.level,
+      semester: details.semester
+    },
+    requireAuth: true, 
+    key: ['user', 'filteredCourses', details.semester, details.level],
+    select: (data) => data?.data,
+    enabled: !!details.semester
+  })
+
+  React.useEffect(() => {
+    if (filteredCourses) {
+      setCourses(filteredCourses?.results)
+    }
+  }, [filteredCourses])
+
+
+  const columns: ITableColumn[] = [
+    {
+      name: 'select',
+      label: 'Register',
+      extra: true,
+      custom: (value, meta) => {
+        return (
+          <>
+          <input onClick={() => handleSelect(meta)} checked={meta?.register} type="checkbox" className='' name="" id="" />
+          </>
+        )
+      }
+    },
+    {
+      name: 'code',
+      label: 'Course Code',
+    },
+    {
+      name: 'title',
+      label: 'Course Title',
+    },
+    {
+      name: 'unit',
+      label: 'Course Units',
+    },
+    {
+      name: 'Jr',
+      label: 'Course Type',
+    }
+  ]
+
+  const done = () => {
+    setModalOpen(false)
+    router.push("/courses")
+  }
+   
+
+  return (
+    <div className='p-4 overflow-y-auto'>
+      {registerCoursesMutation?.isLoading && <Loader />}
+      {
+        modalOpen &&
+        <div className='fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full p-4 bg-black/50'>
+          <div className='min-w-[200px] min-h-40 bg-white rounded-md flex flex-col items-center text-center gap-8 p-8 md:p-16'>
+            <Image src={CheckImg} alt={"check image"} />
+            <div className="flex flex-col gap-3">
+              <h1 className='text-2xl font-semibold'>
+                Courses Registered succesfully
+              </h1>
+              <p className='text-xs'>Hi Jonathan<br />You have successfully created your courses</p>
+            </div>
+            <Link href={"/courses"} className='flex items-center justify-center w-full gap-2 p-3 pl-5 pr-6 text-sm text-white rounded-sm bg-primary'>
+                  Continue
+            </Link>
+          </div>
+        </div>
+      }
+      <div className="flex items-center justify-between gap-4 mb-12">
+          <h1 className='text-2xl font-semibold'>Courses</h1>
+      </div>
+      <div className="p-4 pb-12 bg-white rounded-md">
+        <h3 className="mb-8 text-lg font-semibold md:mb-12">
+            Bio Data
+        </h3>
+        <div className="mb-8">
+          <div className='grid gap-2'>
+            <div className={`grid md:grid-cols-2 text-xs w-full border rounded-sm border-[#A7E1EA] bg-[#ECF5FF]`}>
+              <div className={`flex items-center gap-4 p-2 py-3 border-r border-[#A7E1EA]`}>
+                  <div>
+                      Full Name:
+                  </div>
+                  <div>
+                      {data?.user?.last_name} {data?.user?.first_name} {data?.user?.middle_name}
+                  </div>
+              </div>
+              <div className='flex items-center gap-4 p-2'>
+                  <div>
+                      Level:
+                  </div>
+                  <div>
+                    {data?.user?.level} Level
+                  </div>
+              </div>
+            </div>
+            <div className={`grid md:grid-cols-2 text-xs w-full border rounded-sm border-[#A7E1EA]`}>
+              <div className={`flex items-center gap-4 p-2 py-3 border-r border-[#A7E1EA]`}>
+                  <div>
+                    Matric Number:
+                  </div>
+                  <div>
+                    {data?.user?.matric_no}
+                  </div>
+              </div>
+              <div className='flex items-center gap-4 p-2'>
+                  <div>
+                    Student Type:
+                  </div>
+                  <div>
+                    Undergraduate
+                  </div>
+              </div>
+            </div>
+            <div className={`grid md:grid-cols-2 text-xs w-full border rounded-sm border-[#A7E1EA] bg-[#ECF5FF]`}>
+              <div className={`flex items-center gap-4 p-2 py-3 border-r border-[#A7E1EA]`}>
+                  <div>
+                    Department:
+                  </div>
+                  <div>
+                    {data?.user?.department || "N/A"}
+                  </div>
+              </div>
+              <div className='flex items-center gap-4 p-2'>
+                  <div>
+                      Session:
+                  </div>
+                  <div>
+                    {data?.user?.session || "N/A"}
+                  </div>
+              </div>
+            </div>
+            <div className={`grid md:grid-cols-2 text-xs w-full border rounded-sm border-[#A7E1EA]`}>
+              <div className={`flex items-center gap-4 p-2 py-3 border-r border-[#A7E1EA]`}>
+                  <div>
+                    Option:
+                  </div>
+                  <div>
+                    {data?.user?.option || 'N/A'}
+                  </div>
+              </div>
+              <div className='flex items-center gap-4 p-2'>
+                  <div>
+                    Faculty:
+                  </div>
+                  <div>
+                    {data?.user?.faculty || "N/A"}
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <Table total_units={filteredCourses?.total_units || 0} total_courses={filteredCourses?.total_courses || 0} data={courses || []} columns={columns} />
+          <div className='flex justify-end'>
+            <button onClick={handleRegister} type='submit' className='flex items-center justify-center gap-2 p-2 pl-5 pr-6 text-sm text-white bg-primary'>
+              Register Courses
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ConfirmCourse
--- src/app/(student)/courses/page.tsx
+++ src/app/(student)/courses/page.tsx
@@ -1,6 +1,9 @@
 'use client'
-import Table from '@/components/Table'
-import { ITableColumn } from '@/interfaces'
+import Table from '@/components/Table/User'
+import useFetch from '@/hooks/useFetch'
+import { IProfile, ITableColumn } from '@/interfaces'
+import { apiGetUser } from '@/services/AuthService'
+import { apiGetCourses, apiGetRegisteredCourses } from '@/services/UserService'
 import Link from 'next/link'
 import React from 'react'
 import { MdAdd } from 'react-icons/md'
@@ -9,15 +12,15 @@ import { MdAdd } from 'react-icons/md'
 const StudentCourses = () => {
   const columns: ITableColumn[] = [
     {
-      name: 'Nick',
+      name: 'code',
       label: 'Course Code',
     },
     {
-      name: 'Jr',
+      name: 'title',
       label: 'Course Title',
     },
     {
-      name: 'Jr',
+      name: 'unit',
       label: 'Course Units',
     },
     {
@@ -26,53 +29,62 @@ const StudentCourses = () => {
     }
   ]
 
-  const data = [
-    {
-      Nick: 'I am Nick',
-      Jr: "Is rough"
-    },
-    {
-      Nick: 'Roses are red',
-      Jr: "sTAY wITH ME"
-    },
-  ]
+  const { data: courses, error, isLoading, isFetching, remove, refetch, fetchStatus } = useFetch({
+    api: apiGetCourses, 
+    requireAuth: true, 
+    key: ['user', 'dashboardCourses'] 
+  })
+
+
+  const { data: registeredCourses } = useFetch({
+    api: apiGetRegisteredCourses, 
+    requireAuth: true, 
+    select: ((d: any) => d?.data),
+    key: ['user', 'registeredCourses'] 
+  })
+
+  console.log({
+    courses,
+    registeredCourses
+  })
+
 
   return (
-    <div className='p-4 md:p-6 overflow-y-auto'>
-      <div className="flex items-center gap-4 justify-between mb-8">
+    <div className='p-4 overflow-y-auto md:p-6'>
+      <div className="flex items-center justify-between gap-4 mb-8">
         <h1 className='text-2xl font-semibold'>Courses</h1>
-        <Link href={'/courses/register'} className='flex items-center gap-2 bg-primary p-2 pr-3 text-xs md:text-sm text-white w-fit'>
+        <Link href={'/courses/register'} className='flex items-center gap-2 p-2 pr-3 text-xs text-white bg-primary md:text-sm w-fit'>
           <MdAdd className='text-2xl' />
           Register Course
         </Link>
       </div>
-      <div className="bg-white p-4 pb-12 rounded-md">
-        <h3 className="text-lg mb-8 md:mb-12 font-semibold">
+      <div className="p-4 pb-12 bg-white rounded-md">
+        <h3 className="mb-8 text-lg font-semibold md:mb-12">
           Course Registration
         </h3>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 mb-8 md:mb-16 text-white">
-            <div className='bg-primary rounded-md p-4 flex-1 flex flex-col gap-12'>
+        <div className="grid gap-10 mb-8 text-white md:grid-cols-2 lg:grid-cols-3 md:mb-16">
+            <div className='flex flex-col flex-1 gap-12 p-4 rounded-md bg-primary'>
               <p className='pr-12'>Courses Registered in All Semester</p>
-              <div className="flex justify-between gap-4 items-center mb-2">
-                <h6 className='font-semibold text-5xl'>65</h6>
+              <div className="flex items-center justify-between gap-4 mb-2">
+                <h6 className='text-5xl font-semibold'>{courses?.total_courses}</h6>
                 <button className='p-1 px-2 text-sm text-white'>
                   View
                 </button>
               </div>
             </div>
             <div className='bg-[#43A2B1] rounded-md p-4 flex-1 flex flex-col gap-12'>
-              <p className='pr-12'>Courses Registered in All Semester</p>
-              <div className="flex justify-between gap-4 items-center mb-2">
-                <h6 className='font-semibold text-5xl'>10</h6>
+              <p className='pr-12'>Courses Registered in the Current Session</p>
+              <div className="flex items-center justify-between gap-4 mb-2">
+                <h6 className='text-5xl font-semibold'>{courses?.total_courses_current_semester}</h6>
                 <button className='p-1 px-2 text-sm text-white'>
                   View
                 </button>
               </div>
             </div>
             <div className='bg-[#C65E34] rounded-md p-4 flex-1 flex flex-col gap-12'>
-              <p className='pr-12'>Courses Registered in All Semester</p>
-              <div className="flex justify-between gap-4 items-center mb-2">
-                <h6 className='font-semibold text-5xl'>122</h6>
+              <p className='pr-12'>Total Units Registered in All Semesters</p>
+              <div className="flex items-center justify-between gap-4 mb-2">
+                <h6 className='text-5xl font-semibold'>{courses?.total_units}</h6>
                 <button className='p-1 px-2 text-sm text-white'>
                   View
                 </button>
@@ -80,7 +92,7 @@ const StudentCourses = () => {
             </div>
         </div>
         <div>
-          <Table data={data} columns={columns} />
+          <Table total_units={courses?.total_units || 0} total_courses={courses?.total_courses || 0} data={registeredCourses?.results || []} columns={columns} />
         </div>
       </div>
     </div>
